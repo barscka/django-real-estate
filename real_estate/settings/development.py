9a0a6ce249a9ed504d594265dcc83cb7e86c7876
Alisson Oliveira Ferreira <alissonof@uol.com.br>
2022-10-16 20:29:45 -0300

    jwt e djoser auth, email

--- real_estate/settings/development.py
+++ real_estate/settings/development.py
@@ -1,6 +1,17 @@
+from django.conf.global_settings import DEFAULT_FROM_EMAIL, EMAIL_BACKEND, EMAIL_HOST_USER
 from .base import *
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST=env("EMAIL_HOST")
+EMAIL_USE_TLS=True
+EMAIL_PORT=env("EMAIL_PORT")
+EMAIL_HOST_USER=env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD=env("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL="alissonof@uol.com.br"
+DOMAIN=env("DOMAIN")
+SITE_NAME="REAL ESTATE"
 
 DATABASES = {
     "default": {
--- real_estate/urls.py
+++ real_estate/urls.py
@@ -1,10 +1,15 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
+
 urlpatterns = [
-    path('supercrect/', admin.site.urls),    
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('supercrect/', admin.site.urls),
+    path("api/v1/auth/", include("djoser.urls")),
+    path("api/v1/auth/", include("djoser.urls.jwt"))
+    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 
 admin.site.site_header = "Real Estate Admin"
